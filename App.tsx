2c50bf36f440d5b43a53d53d8c41ef584d436a49
henrymoulton <30093692+henrymoulton@users.noreply.github.com>
2023-08-05 15:41:53 +0100

    feat: create reproducible bug

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PagerView from "react-native-pager-view";
+import { Image as ExpoImage} from "expo-image"
 
+const images = [
+    "https://www.datocms-assets.com/54483/1650389131-main-background-img-sofar-sounds.jpg?auto=enhance&fm=webp",
+    "https://www.datocms-assets.com/54483/1685045447-main-background-img-orasay.jpg?auto=enhance&fm=webp",
+    "https://www.datocms-assets.com/54483/1659117336-main-background-img-koya.jpg?auto=enhance&fm=webp"
+]
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <PagerView style={{ flex: 1 }}>
+        {images.map(url => <View key={url} style={{ flex:1 }}>
+          <ExpoImage style={{ width: "100%", height: "100%"}} contentPosition={"center"} contentFit={"cover"}  source={{ uri: url}} />
+        </View>)}
+
+      </PagerView>
   );
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+# Expo Image - PagerView bug
+
+When using Expo Image and PagerView together it waits for the image to load/render despite PagerView not being lazy. FastImage doesn't have this behaviour.
+
+See screen recording: https://www.loom.com/share/e2c1fce71f494cb3a6d6f9cee7bbd990
